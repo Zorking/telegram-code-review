31a7a89cd6cdae29c472be656227339b19c3c1c8
Vadim Fabrichnov <v.fabrichnov@deliveryweb.ru>
2018-10-03 22:50:13 +0300

    Refactoring

--- send_messages.py
+++ send_messages.py
@@ -15,43 +15,47 @@ apihelper.proxy = {"https": PROXY}
 bot = telebot.TeleBot(token=BOT_API_KEY)
 
 
-def send_msg():
+def define_approvers():
     cursor.execute("SELECT * FROM developer")
     developers = cursor.fetchall()
     projects = PROJECT_IDS
     request_params = {"private_token": GITLAB_TOKEN, "state": "opened"}
-    users = {chat_id: {"username": gitlab_username} for chat_id, _, gitlab_username in developers}
+    approvers = {chat_id: {"username": gitlab_username} for chat_id, _, gitlab_username in developers}
     for project_id in projects:
         url = "{}/v4/projects/{}/merge_requests".format(GITLAB_URL, project_id)
         response = requests.get(url, params=request_params).json()
         for merge_request in response:
-            users = assign_merge_request(developers, merge_request, users)
+            if merge_request.get("work_in_progress"):
+                continue
+            approvers = assign_merge_request(developers, merge_request, approvers)
+    return approvers
 
-    for user_id, messages in users.items():
+
+def send_messages(approvers):
+    for chat_id, messages in approvers.items():
         messages = messages.get("messages")
         if not messages:
             continue
         links = [x.get("web_url") for x in messages]
         message = "\n".join(links)
         message = "Review:\n{}".format(message)
-        bot.send_message(user_id, message)
+        bot.send_message(chat_id, message)
 
 
 def assign_merge_request(developers, merge_request, users):
-    if not merge_request.get("work_in_progress"):
-        assigned_reviewers = []
-        for _ in range(NUMBER_OF_REVIEWERS):
-            user_id, username = None, None
-            while not user_id or is_correct_reviewer(username, merge_request, user_id, assigned_reviewers):
-                user_id = developers[randint(0, len(developers) - 1)][0]
-                username = users[user_id].get("username")
-            users = update_users(users, user_id, merge_request)
-            assigned_reviewers.append(user_id)
+    approvers = []
+    for _ in range(NUMBER_OF_REVIEWERS):
+        chat_id, username = None, None
+        while not chat_id or is_correct_reviewer(username, merge_request, chat_id, approvers):
+            chat_id = developers[randint(0, len(developers) - 1)][0]
+            username = users[chat_id].get("username")
+        users = update_users(users, chat_id, merge_request)
+        approvers.append(chat_id)
     return users
 
 
-def update_users(users, user_id, merge_request):
-    user = users.get(user_id)
+def update_users(users, chat_id, merge_request):
+    user = users.get(chat_id)
 
     if user.get("messages"):
         messages = user.get("messages")
@@ -59,13 +63,14 @@ def update_users(users, user_id, merge_request):
         user["messages"] = messages
     else:
         user["messages"] = [merge_request]
-    users[user_id] = user
+    users[chat_id] = user
     return users
 
 
-def is_correct_reviewer(username, x, user_id, assined_reviewers):
-    return username == x.get("author", {}).get("username") or user_id in assined_reviewers
+def is_correct_reviewer(username, merge_request, chat_id, assined_reviewers):
+    return username == merge_request.get("author", {}).get("username") or chat_id in assined_reviewers
 
 
 if __name__ == "__main__":
-    send_msg()
+    approvers = define_approvers()
+    send_messages(approvers)
